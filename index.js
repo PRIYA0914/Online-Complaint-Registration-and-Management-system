8f2b218f9b35b4358ef8e3805c5bdf447a71effc
Parvesh <parveshahamed00@gmail.com>
2023-02-06 09:47:27 +0530

    adding EJS to application

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static("public")); // public folder for css and images
+app.set("view engine", "ejs"); // seting the engine for ejs
+app.get('/', (req, res) => res.sendFile(__dirname+"/index.html"))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
