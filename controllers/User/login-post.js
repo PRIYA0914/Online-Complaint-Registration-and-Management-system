60fddf8c57c95e81acc41dec30691864144fd27c
Parvesh <parveshahamed00@gmail.com>
2023-02-21 10:03:44 +0530

    adding functioanlity to check, wether the user name already taken or not in User/signup-post

--- controllers/User/login-post.js
+++ controllers/User/login-post.js
@@ -1,3 +1,20 @@
-exports.login_post=(req,res)=>{
-    res.send(req.body)
-}
+const User = require("../../models/Authentication/User-schema");
+const mongoose = require("mongoose");
+exports.login_post = (req, res) => {
+  User.findOne({ username: req.body.username }, (err, docs) => {
+    if (!err) {
+      // console.log(docs);
+      // res.sendStatus(200)
+      console.log(docs);
+      if (docs != null) {
+        console.log("notnull");
+      } else {
+        res.write("<h1>User Not found</h1>");
+        res.write("<h3><a href='/sub-pages/user/signup.html'>Sign up</a></h3>");
+        console.log("null");
+      }
+    } else {
+      res.sendStatus(404);
+    }
+  });
+};
--- models/Authentication/User-schema.js
+++ models/Authentication/User-schema.js
@@ -1,7 +1,9 @@
 const mongoose=require("mongoose")
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    unique: true //  must be unique
+  },
   password: String,
- 
 });
 module.exports=mongoose.model("UserAuth",userSchema)
