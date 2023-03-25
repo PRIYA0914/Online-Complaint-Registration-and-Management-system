79ca367ff392a7dff90420eb975706d778518d5e
Parvesh <parveshahamed00@gmail.com>
2023-03-25 12:08:18 +0530

    adding functionality to open g-map when the button is clicked in complaint register form

--- controllers/User/dashboard.js
+++ controllers/User/dashboard.js
@@ -2,5 +2,6 @@ exports.dashboard=(req,res)=>{
     // res.send("User DashBoard")
     // res.send(req.paras);
     // console.log(req.params.id);
+    // using params.id -- search in database
     res.render("UserDashboard", { userID: req.params.id });
 }
