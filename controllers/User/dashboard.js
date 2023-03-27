a457d251996817a7f6b760b706c4d80935d657d2
Parvesh <parveshahamed00@gmail.com>
2023-03-27 11:45:04 +0530

    fetching username and passing when rendering on user dashboard

--- controllers/User/dashboard.js
+++ controllers/User/dashboard.js
@@ -1,19 +1,30 @@
 const Complaint = require("../../models/Complaint-schema");
+const User = require("../../models/Authentication/User-schema");
 const mongoose = require("mongoose");
-exports.dashboard = (req, res) => {
+exports.dashboard = async (req, res) => {
   // res.send("User DashBoard")
   // res.send(req.paras);
   // console.log(req.params.id);
   // using params.id -- search in database
-//   res.render("UserDashboard", { userID: req.params.id });
-Complaint.find({ userID: req.params.id }, function (err, docs) {
-  if (err) {
-    console.log(err);
+  //   res.render("UserDashboard", { userID: req.params.id });
+  try {
+    var username = await User.find({ userId: req.params.id }).exec();
+    // console.log(username);
+    Complaint.find({ userID: req.params.id }, function (err, docs) {
+      if (err) {
+        console.log(err);
         res.sendStatus(500);
+      } else {
+        res.render("UserDashboard", {
+          complaints: docs,
+          userID: req.params.id,
+          userName: username[0].username.slice(0, 2),
+        });
 
-  } else {
-
-    console.log(docs);
+        // console.log(docs);
+      }
+    });
+  } catch (error) {
+    console.log(error);
   }
-});
 };
