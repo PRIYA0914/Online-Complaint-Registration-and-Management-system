5e22fd2db40cf00315ed17a8f40029f4a4ad759f
Parvesh <parveshahamed00@gmail.com>
2023-04-05 10:16:23 +0530

    creating and adding functionality to display unauthorized html page when admin failed to login

--- controllers/Admin/login-post.js
+++ controllers/Admin/login-post.js
@@ -1,3 +1,15 @@
+const Admin=require("../../models/Authentication/Admin-schema")
+const mongoose=require("mongoose")
 exports.login=(req,res)=>{
-    res.send(req.body)
+    Admin.find((err,docs)=>{
+        if(err){
+            res.sendStatus(500)
+        }else{
+            if (docs[0].name===req.body.name && docs[0].password===req.body.password) {
+                res.sendStatus(200)
+            } else {
+                res.sendFile(__dirname+"/unauthorized.html")
+            }
+        }
+    })
 }
