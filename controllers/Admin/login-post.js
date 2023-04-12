2fe3f167dadd1023365b06d31d575bafa5e6cffa
Parvesh <parveshahamed00@gmail.com>
2023-04-12 09:48:07 +0530

    designed admin dashboard & displaying complaints in home page

--- controllers/Admin/login-post.js
+++ controllers/Admin/login-post.js
@@ -1,12 +1,18 @@
 const Admin=require("../../models/Authentication/Admin-schema")
+const Complaints=require("../../models/Complaint-schema")
 const mongoose=require("mongoose")
+let doc=
+Complaints.find((err,docs)=>{
+    doc=docs
+})
 exports.login=(req,res)=>{
     Admin.find((err,docs)=>{
         if(err){
             res.sendStatus(500)
         }else{
             if (docs[0].name===req.body.name && docs[0].password===req.body.password) {
-                res.sendStatus(200)
+                res.render("AdminDashboard",{complaints:doc})
+                // console.log(doc);
             } else {
                 res.sendFile(__dirname+"/unauthorized.html")
             }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser"); // to handle post requests
 const mongoose = require("mongoose");
-const cookieParser=require("cookie-parser") // to store JWT Token in our cookie storage for getting verified in each routes
+const cookieParser = require("cookie-parser"); // to store JWT Token in our cookie storage for getting verified in each routes
 const fileUplaod = require("express-fileupload");
 
 const app = express();
@@ -9,7 +9,7 @@ require("dotenv").config(); // for environment variables
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.static("public")); // public folder for css and images
 app.set("view engine", "ejs"); // seting the engine for ejs
 //
@@ -18,19 +18,16 @@ app.set("view engine", "ejs"); // seting the engine for ejs
 const uri = process.env.mongoDB;
 function database_connection() {
   try {
-        mongoose.set("strictQuery", false);
+    mongoose.set("strictQuery", false);
 
-    mongoose.connect(uri,()=>{
+    mongoose.connect(uri, () => {
       console.log("Successfully connected to DB");
-    })
-
+    });
   } catch (error) {
     console.log(`error in database connection ${error}`);
-
-
   }
 }
-database_connection()
+database_connection();
 app.use(
   fileUplaod({
     useTempFiles: true,
@@ -44,7 +41,7 @@ app.use("/user", require("./routes/User/dashboard"));
 app.use("/", require("./routes/User/complaintForm-get"));
 app.use("/", require("./routes/User/complaintForm-post"));
 // deleting user-complaint
-app.use("/",require("./routes/User/deleteComplaint"))
+app.use("/", require("./routes/User/deleteComplaint"));
 app.use("/officer/login", require("./routes/Officer/login-post"));
 app.use("/officer/signup", require("./routes/Officer/signup-post"));
 app.use("/admin/login", require("./routes/Admin/login-post"));
@@ -64,7 +61,7 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 // app.post("/add_user", async (request, response) => {
 //     const user = new userModel(request.body);
-  
+
 //     try {
 //       await user.save();
 //       response.send(user);
